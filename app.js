5d34e80711d069acc20a1ee498a1e2340cef10f0
Luis Iván García Luna <ligl_adobe@hotmail.com>
2015-05-30 14:50:57 -0500

    Change in the Port of node

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var port = Number(process.env.PORT || 8080);
+var port = Number(process.env.PORT || 3000);
 
 app.set("view engine", "jade");
 
