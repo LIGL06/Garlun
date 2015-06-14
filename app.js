e2cb072c36b244afde0def6182a1ecceef06cdb9
Luis Iván García Luna <ligl_adobe@hotmail.com>
2015-06-14 10:28:31 -0500

    Add some display to console on port in app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var app = express();
 var port = Number(process.env.PORT || 3000);
 
@@ -8,9 +7,7 @@ app.set("view engine", "jade");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    
-    
     res.render("index");
 });
-
 app.listen(port);
+console.log("nodeJS running in port "+ port);
